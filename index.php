525a8e3104b16192d60e748264bbf9ab148a194b
Philippe Traverse <philippe.traverse@gmail.com>
2012-12-20 23:09:50 -0800

    fixed the link again!!

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 echo "helloworld in PHP!";
 ob_start();
 
-echo '<a href="/needleman-wunsch/index.php">';
+echo '<a href="/needleman-wunsch/example.php">';
 echo '<h2>';
 echo 'NW';
 echo '</h2>';
