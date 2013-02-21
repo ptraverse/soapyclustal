f9cb95bc22a23ba0c52fdf5ace29c5766229a1c5
Philippe Traverse <philippe.traverse@gmail.com>
2013-02-21 01:15:48 -0800

    Added a css and beefed up client and spec TODO- alter http post spec, add http_get_server.php and http_post_server.php

--- helloworld_client/client.php
+++ helloworld_client/client.php
@@ -1,6 +1,6 @@
 <?php
 try{
-  $sClient = new SoapClient('http://localhost/new_soapy/helloworld_server/hello.xml');
+  $sClient = new SoapClient("http://localhost/new_soapy/helloworld_server/hello.xml");
   
   
   
@@ -19,14 +19,16 @@ try{
   echo '<form action="client.php">';
   echo 'yourName: <input placeholder="'.$params.'" name="yourName" type="text">';
   echo '<input type="submit" value="Submit">';
-  
   echo "<hr>";
+  
   echo "<h1>Output</h1>";
   echo "<pre>";
   var_dump($response);
   echo "</pre>";
   echo "<hr>";
-   
+  
+  echo '<h1>Description</h1>';
+  echo "The hello world service is meant as both a demonstration and a template for the other web based services in the project scope.";
   
 } catch(SoapFault $e){
 	echo __FILE__.":".__LINE__;
